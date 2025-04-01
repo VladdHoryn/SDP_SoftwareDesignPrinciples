ce19985171d3a438dc20e211bd0599447017b8c4
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-04-01 16:12:22 +0300

    Laba 6 Ex_3

---
+++ Labs/Laba_6/Ex_3/WpfApp1/WpfApp1/City.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Threading.Tasks.Dataflow;
+
+namespace WpfApp1;
+
+public class City: IComparable, IComparer
+{
+    private string name;
+    private double area;
+    private int population;
+    
+    private List<City> cities = new List<City>();
+    
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    
+    public double Area
+    {
+        get { return area; }
+        set { area = value; }
+    }
+    
+    public int Population
+    {
+        get { return population; }
+        set { population = value; }
+    }
+    
+    public City(string name, double area, int population)
+    {
+        this.name = name;
+        this.area = area;
+        this.population = population;
+    }
+
+    public int CompareTo(object? obj)
+    {
+        if (obj == null)
+            return 1;
+
+        if (!(obj is City otherCity))
+            throw new ArgumentException("Object is not a City");
+
+        if (this.area > otherCity.area)
+            return 1;
+        else if (this.area < otherCity.area)
+            return -1;
+        else
+            return 0;
+    }
+
+    public int Compare(object? x, object? y)
+    {
+        if (!(x is City firstCity) || !(y is City secondCity))
+            throw new ArgumentException("Object is not a City");
+
+        if (firstCity.area > secondCity.area)
+            return 1;
+        else if (firstCity.area < secondCity.area)
+            return -1;
+        else
+        {
+            if (firstCity.population > secondCity.population)
+                return 1;
+            else if (firstCity.population < secondCity.population)
+                return -1;
+            else
+                return 0;
+        }
+    }
+    
+    public void AddCity(City city)
+    {
+        cities.Add(city);
+    }
+    
+    public IEnumerator GetEnumerator()
+    {
+        cities.Sort((city1, city2) => city1.Population.CompareTo(city2.Population));
+        
+        foreach (var city in cities)
+        {
+            yield return city;
+        }
+    }
+
+    public string GetCities()
+    {
+        string result = "";
+        foreach (var i in cities)
+        {
+            result += $"Name: {i.name}, Area: {i.area}, Population: {i.population}\n";
+        }
+
+        return result;
+    }
+}
---
+++ Labs/Laba_6/Ex_3/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ShowText(object sender, RoutedEventArgs e)
+    {
+        City city1 = new City("Kyiv", 839.0, 2804000);
+        City city2 = new City("Lviv", 182.0, 720000);
+        City city3 = new City("Odesa", 236.0, 1000000);
+
+        // Додаємо їх у колекцію
+        city1.AddCity(city1);
+        city1.AddCity(city2);
+        city1.AddCity(city3);
+        string line = "";
+        foreach (var city in city1)
+        {
+           line += $"City: {((City)city1).Name}, Population: {((City)city).Population}\n";
+        }
+        TextBox.Text = city1.GetCities();
+    }
+}
